df92c6496026e69cc6ebc9a0a5806612aa7c7315
Shady <shadyrajaab@gmail.com>
2022-01-08 17:26:53 -0300

    feat: added command log (reason)

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -7,22 +7,23 @@ import {
   PermissionString,
   UserResolvable,
 } from 'discord.js';
-import { Command } from '../structures/structures/command';
-import { notAdmin, notConnected, notOwner } from '../translations/temporarychannels/globalMessages';
-import { ProfileManager } from '../structures/managers/profileManager';
-import { GuildManager } from '../structures/managers/guildManager';
-import { fetchCommandData } from '../utils/fetchFunctions';
-import { onlyGuilds, needPermission, unavailableCommand } from '../translations/interactionMessages';
-import { permissionsRemoved } from '../translations/globalMessages';
-import { Language } from '../structures/structures/types';
-import { Room } from '../structures/managers/roomManager';
+import {Command} from '../structures/structures/command';
+import {notAdmin, notConnected, notOwner} from '../translations/temporarychannels/globalMessages';
+import {ProfileManager} from '../structures/managers/profileManager';
+import {GuildManager} from '../structures/managers/guildManager';
+import {fetchCommandData} from '../utils/fetchFunctions';
+import {onlyGuilds, needPermission, unavailableCommand} from '../translations/interactionMessages';
+import {permissionsRemoved} from '../translations/globalMessages';
+import {Language} from '../structures/structures/types';
+import {Room} from '../structures/managers/roomManager';
 
 const unavailableCommands = ['lyrics', 'profile', 'help'];
 
 export default (client: Client, commands: Array <Command>) => {
   client.on('interactionCreate', async (interaction: Interaction) => {
     if (!interaction.isCommand()) return;
-    const { commandName } = interaction;
+    const {commandName} = interaction;
+    const reason = `🔧 /${commandName}: Requested by: ${interaction.user} | ID: ${interaction.user.id}`;
     const guild = interaction.guild as Guild;
     const member = interaction.member as GuildMember;
     const command = commands.find((command) => command.name === commandName);
@@ -30,9 +31,15 @@ export default (client: Client, commands: Array <Command>) => {
     const profileManager = new ProfileManager(interaction.user);
     const guildManager = new GuildManager(guild);
     await fetchCommandData(commandName);
-    const language = await profileManager.getLanguage() || await guildManager.getLanguage() as Language;
-    if (!guild && !command?.direct) return interaction.reply(onlyGuilds((language) || 'english'));
-    if (!guild && command?.direct) try { return command?.execute(interaction, (language) || 'english'); } catch (err) { console.log(err); }
+    const language = await profileManager.getLanguage() || await guildManager.getLanguage() as Language || 'english';
+    if (!guild && !command?.direct) return interaction.reply(onlyGuilds((language)));
+    if (!guild && command?.direct) {
+      try {
+        return command?.execute(interaction, language, reason);
+      } catch (err) {
+        console.log(err);
+      }
+    }
     const botPermissions = (await interaction.guild?.members.fetch(interaction.client.user?.id as UserResolvable))?.permissionsIn(interaction.channel as GuildChannelResolvable).toArray();
     if (unavailableCommands.find((command: string) => command === commandName)) {
       return interaction.reply({
@@ -43,7 +50,7 @@ export default (client: Client, commands: Array <Command>) => {
     if (commandPermission) {
       for (const permissionName of commandPermission) {
         if (permissionName === 'TRCHANNEL_OWNER' || permissionName === 'TRCHANNEL_ADMIN') {
-          const { voice } = member;
+          const {voice} = member;
           if (!voice.channel || !await Room.isRoom(voice.channel!)) {
             return interaction.reply({
               content: notConnected(language),
@@ -76,6 +83,10 @@ export default (client: Client, commands: Array <Command>) => {
         }
       }
     }
-    try { return command?.execute(interaction, language); } catch (err) { console.log(err); }
+    try {
+      return command?.execute(interaction, language, reason);
+    } catch (err) {
+      console.log(err);
+    }
   });
 };
