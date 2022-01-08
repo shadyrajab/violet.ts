cfddb4c70e1ae5c6bc6b397c28b030cdc7f1b660
Shady <shadyrajaab@gmail.com>
2022-01-08 16:41:06 -0300

    fix: fixing unknow member error

---
+++ commands/moderation/trdelete.ts
@@ -0,0 +1,56 @@
+import {Client, VoiceChannel} from 'discord.js';
+import {GuildManager} from '../../structures/managers/guildManager';
+import {Room} from '../../structures/managers/roomManager';
+import {Command} from '../../structures/structures/command';
+import {finished} from '../../translations/globalMessages';
+import {notEnabled, pleaseSelect} from '../../translations/moderation/trdeleteMessages';
+
+export class TRDelete extends Command {
+  constructor(client: Client) {
+    super(new Command({
+      client: client,
+
+      name: 'trdelete',
+      description: 'Moderation • Delete all the temporary channels created in this server.',
+      permissions: ['ADMINISTRATOR'],
+      options: [{
+        name: 'channel',
+        description: 'If you want to move all the users to a new channel, what is the channel?',
+        required: false,
+        type: 'CHANNEL',
+      }],
+
+      execute: async (interaction, language) => {
+        const guildManager = new GuildManager(interaction.guild!);
+        const newChannel = interaction.options.getChannel('channel');
+        const {trcategory, trchannel} = await guildManager.getTrChannels();
+        if (!trcategory && !trchannel) {
+          return interaction.reply({
+            content: notEnabled(language),
+            ephemeral: true,
+          });
+        }
+        if (newChannel && !(newChannel instanceof VoiceChannel)) {
+          return interaction.reply({
+            content: pleaseSelect(language),
+            ephemeral: true,
+          });
+        } else {
+          for (const channel of trcategory!.children) {
+            const voiceChannel = channel[1];
+            if (voiceChannel instanceof VoiceChannel && await Room.isRoom(voiceChannel)) {
+              if (newChannel) for (const member of voiceChannel.members) member[1].edit({channel: newChannel});
+              setTimeout( () => {
+                voiceChannel.delete(`Deleting all the temporary channels. Requested by: ${interaction.user.username}`);
+              }, 3000);
+            }
+          }
+        }
+        await interaction.reply({
+          content: finished(language),
+          ephemeral: true,
+        });
+      },
+    }));
+  }
+}
